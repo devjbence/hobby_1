4e97253366247b97007e881e4b61f68a1d76ef16
devjbence <strongerh@gmail.com>
2020-04-10 14:18:24 +0200

    teszt környezet behozása https://trello.com/c/Am6coTGZ/5-teszt-k%C3%B6rnyezet-behoz%C3%A1sa

--- Hobby_1/Startup.cs
+++ Hobby_1/Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Logic.Services;
+﻿using Logic.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using StructureMap;
 using View;
 
